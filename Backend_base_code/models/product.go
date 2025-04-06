2480ef2f98f544f34ee0006b70d89b123fa92ea2
Peter Adranly <106859213+PeterAdr14@users.noreply.github.com>
2025-04-06 14:27:31 -0700

    Update product.go

--- Backend_base_code/models/product.go
+++ Backend_base_code/models/product.go
@@ -1,9 +1,16 @@
 package models
 
 type Recipe struct {
-	Id         int `gorm:"primaryKey;autoIncrement"`
-	User_Id    string
-	Output     string
-	Preference string
-	Allergy    string
+	Id         int    `gorm:"primaryKey;autoIncrement"`
+	User_Id    int    `json:"user_id"`
+	Title      string `json:"title"`
+	Output     string `json:"output"`
+	Preference string `json:"preference"`
+	Allergy    string `json:"allergy"`
+}
+
+type User struct {
+	Id    int    `gorm:"primaryKey;autoIncrement"`
+	User  string `json:"user"`
+	Email string `json:"email"`
 }
