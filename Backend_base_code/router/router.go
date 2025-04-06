93f2cf2be99034686cfab85a865121778ef03f3d
Peter Adranly <106859213+PeterAdr14@users.noreply.github.com>
2025-04-06 14:29:17 -0700

    Update router.go

--- Backend_base_code/router/router.go
+++ Backend_base_code/router/router.go
@@ -1,7 +1,9 @@
 package router
 
 import (
-	"app/controllers"
+	"log"
+
+	"getrecipe.local/get_recipe/controllers"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,5 +17,17 @@ func SetupRouter() {
 	router.Use(cors.New(corsConfig))
 	router.Group("/get").
 		GET("/recipes/:user_id", productController.Get)
+
+	createGroup := router.Group("/create")
+	{
+		createGroup.POST("/recipe/:user_id", productController.Create_Recipe)
+		createGroup.POST("/user", productController.Create_User)
+	}
+	router.Group("/delete").
+		DELETE("/recipe/:user_id/:recipe_id", productController.Delete)
+
+	router.Group("/health").
+		GET("", productController.Health)
 	router.Run()
+	log.Println("main/router: executed")
 }
