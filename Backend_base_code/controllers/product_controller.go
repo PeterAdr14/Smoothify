752d9dce8efe617bdc95248067186771efe74269
Peter Adranly <106859213+PeterAdr14@users.noreply.github.com>
2025-04-06 14:27:10 -0700

    Update product_controller.go

--- Backend_base_code/controllers/product_controller.go
+++ Backend_base_code/controllers/product_controller.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
-	"app/config"
-	"app/models"
+	"log"
+	"reflect"
+
 	"net/http"
+
+	"getrecipe.local/get_recipe/config"
+	"getrecipe.local/get_recipe/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,55 +15,142 @@ import (
 type ProductController struct {
 }
 
-// func (con *ProductController) Index(c *gin.Context) {
-// 	var recipes []models.Recipe
-// 	config.DB.Find(&recipes)
-// 	c.JSON(http.StatusOK, recipes)
-// }
-
+// GET the recipes for the user
 func (con *ProductController) Get(c *gin.Context) {
-	var recipe models.Recipe
-	userID := c.Param("user_id")
+	var recipes []models.Recipe
+	var user models.User
 
-	// Query the database
-	if err := config.DB.Where("user_id = ?", userID).Order("id").First(&recipe).Error; err != nil {
+	userID := c.Param("user_id")
+	log.Println("product_controller|| user_id: ?", userID, reflect.TypeOf(userID))
+
+	//User Table Database Execution
+	err_user := config.DB.Raw("select * from user where user = ?", userID).Find(&user).Error
+
+	log.Println(user)
+	if err_user != nil {
+		log.Println("main/router/product_controller: query/user error|| error: ?", err_user)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+		return
+	} else if len(user.User) == 0 {
+		log.Println("main/router/product_controller: user doesn't exist")
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	//Recipe Table Database Execution
+	err_recipe := config.DB.Raw("select * from recipe where user_id = ?", user.Id).Find(&recipes).Error
+	if err_recipe != nil {
+		log.Println("main/router/product_controller: query/recipe error|| error: ?", err_recipe)
 		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
 		return
 	}
 
-	// Return the recipe
-	c.JSON(http.StatusOK, recipe)
+	log.Println("recipe: ", recipes)
 
-	// config.DB.First(&recipe, c.Params.ByName("user_id"))
-	// c.JSON(http.StatusOK, recipe)
+	if len(recipes) == 0 {
+		log.Println("main/router/product_controller: empty response|| response: ?", recipes)
+	}
+
+	// Return the recipe
+	log.Println("main/router/product_controller: executed")
+
+    response := gin.H{
+		"user":    user.Id,
+		"recipes": recipes,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
-// func (con *ProductController) Create(c *gin.Context) {
-// 	var recipe models.Recipe
-// 	c.BindJSON(&recipe)
-// 	if err := config.DB.Create(&recipe).Error; err != nil {
-// 		c.AbortWithError(http.StatusBadRequest, err)
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, recipe)
-// }
+func (con *ProductController) Health(c *gin.Context) {
+	c.JSON(http.StatusOK, "Ok")
+}
 
-// func (con *ProductController) Update(c *gin.Context) {
-// 	var recipe models.Recipe
-// 	c.BindJSON(&recipe)
-// 	recipe.Id, _ = strconv.Atoi(c.Params.ByName("id"))
-// 	if err := config.DB.Updates(&recipe).Error; err != nil {
-// 		c.AbortWithError(http.StatusBadRequest, err)
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, recipe)
-// }
+// CREATES a new recipe for a user
+func (con *ProductController) Create_Recipe(c *gin.Context) {
+	var recipe models.Recipe
+	c.BindJSON(&recipe)
+    var user models.User
 
-// func (con *ProductController) Delete(c *gin.Context) {
-// 	var recipe models.Recipe
-// 	if err := config.DB.Delete(&recipe, c.Params.ByName("id")).Error; err != nil {
-// 		c.AbortWithError(http.StatusBadRequest, err)
-// 		return
-// 	}
-// 	c.Status(http.StatusOK)
-//}
+	userID := c.Param("user_id")
+	log.Println("product_controller|| user_id: ?", userID, reflect.TypeOf(userID))
+
+	//User Table Database Execution
+	err_user := config.DB.Raw("select * from user where user = ?", userID).Find(&user).Error
+
+	log.Println(user)
+	if err_user != nil {
+		log.Println("main/router/product_controller: query/user error|| error: ?", err_user)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+		return
+	} else if len(user.User) == 0 {
+		log.Println("main/router/product_controller: user doesn't exist")
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	err := config.DB.Exec("insert into recipe (user_id, title, output, preference, allergy) values (?, ?, ?, ?, ?)", recipe.User_Id, recipe.Title, recipe.Output, recipe.Preference, recipe.Allergy).Error
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, recipe)
+}
+
+// CREATES a new user
+func (con *ProductController) Create_User(c *gin.Context) {
+	var dupe models.User
+	var user models.User
+	c.BindJSON(&user)
+
+	exist := config.DB.Raw("select * from user where user = ?", user.User).Find(&dupe).Error
+	if exist != nil || user.User == dupe.User {
+		c.JSON(http.StatusFound, "User Exists")
+		return
+	}
+
+	err := config.DB.Exec("insert into user (user, email) values (?, ?)", user.User, user.Email).Error
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+// DELETE a recipe
+func (con *ProductController) Delete(c *gin.Context) {
+    var user models.User
+
+	recipeID := c.Param("recipe_id")
+	log.Println("product_controller|| recipe_id: ?", recipeID, reflect.TypeOf(recipeID))
+
+	userID := c.Param("user_id")
+	log.Println("product_controller|| user_id: ?", userID, reflect.TypeOf(userID))
+
+	//User Table Database Execution
+	err_user := config.DB.Raw("select * from user where user = ?", userID).Find(&user).Error
+
+	log.Println(user)
+	if err_user != nil {
+		log.Println("main/router/product_controller: query/user error|| error: ?", err_user)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+		return
+	} else if len(user.User) == 0 {
+		log.Println("main/router/product_controller: user doesn't exist")
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	//Recipe Table Database Execution
+	err_recipe := config.DB.Exec("delete from recipe where id = ?", recipeID).Error
+	if err_recipe != nil {
+		log.Println("main/router/product_controller: query/recipe error|| error: ?", err_recipe)
+		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
+		return
+	}
+	// Return the recipe
+	log.Println("main/router/product_controller: executed")
+	c.JSON(http.StatusOK, "Successfully Deleted")
+}
